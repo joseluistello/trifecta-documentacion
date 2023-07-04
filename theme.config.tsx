08e780c1a8b666c7745c899b4682bc4335649a02
joseluistello <jltello96@gmail.com>
2023-07-04 01:46:31 -0600

    Large

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,13 +1,110 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import type { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig } from 'nextra-theme-docs'
+
+const logo = (
+  <><span className="nx-font-extrabold">Trifecta</span><span 
+  className="nx-ml-2 nx-hidden nx-font-normal nx-text-gray-600 md:nx-inline">
+    Analiza tus datos en segundos
+  </span></>
+)
 
 const config: DocsThemeConfig = {
-  logo: <span>Trifecta</span>,
   project: {
-    link: 'https://ai.trifecta.fyi/',
+    link: 'https://github.com/joseluistello/trifecta-documentacion'
   },
-  chat: {
-    link: 'https://ai.trifecta.fyi/',
+  docsRepositoryBase: 'https://github.com/joseluistello/trifecta-documentacion',
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Trifecta'
+      }
+    }
+  },
+  logo,
+  head: function useHead() {
+    const { title } = useConfig()
+    const { route } = useRouter()
+    const socialCard =
+      route === '/' || !title
+        ? 'https://nextra.site/og.jpeg'
+        : `https://nextra.site/api/og?title=${title}`
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta property="twitter:image" content="https://opengraphs.s3.us-east-2.amazonaws.com/opengraph.jpg"/>
+        <meta
+          name="description"
+          content="Documentacion de Trifecta."
+        />
+        <meta
+          name="og:description"
+          content="Documentacion de Trifecta."
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={socialCard} />
+        <meta name="twitter:site:domain" content="nextra.site" />
+        <meta name="twitter:url" content="https://documentacion.trifecta.fyi" />
+        <meta
+          name="og:title"
+          content={title ? title + ' – Trifecta' : 'Trifecta'}
+        />
+        <meta name="og:image" content={socialCard} />
+        <meta name="apple-mobile-web-app-title" content="Trifecta" />
+        <link rel="icon" href="public/favicon.ico" type="image/png" />
+        <link
+          rel="icon"
+          href="/favicon-dark.svg"
+          type="image/svg+xml"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="icon"
+          href="/favicon-dark.png"
+          type="image/png"
+          media="(prefers-color-scheme: dark)"
+        />
+      </>
+    )
+  },
+  // banner: {
+  //   key: '2.0-release',
+  //   text: (
+  //     <a href="https://nextra.site" target="_blank" rel="noreferrer">
+  //       🎉 Nextra 2.0 is released. Read more →
+  //     </a>
+  //   )
+  // },
+  editLink: {
+    text: 'Edit this page on GitHub →'
+  },
+  feedback: {
+    content: 'Question? Give us feedback →',
+    labels: 'feedback'
+  },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>
+      }
+      return <>{title}</>
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
+  },
+  footer: {
+    text: (
+      <div className="flex w-full flex-col items-center sm:items-start">
+        <p className="mt-6 text-xs">
+          © {new Date().getFullYear()} Design and create by Shu Ding.
+        </p>
+      </div>
+    )
   }
 }
 
